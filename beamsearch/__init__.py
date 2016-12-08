20a7875a5af15304db293455d6f7d8b03f52a707
Marten Kenbeek <marten.knbk@gmail.com>
2016-12-09 00:05:11 +0100

    Added additional evaluation methods.

--- beamsearch/__init__.py
+++ beamsearch/__init__.py
@@ -1,7 +1,8 @@
 from .load_arff import load_data, get_data
 from .beam_search import BeamSearch, search
-from .metrics import weighted_relative_accuracy
+from .metrics import weighted_relative_accuracy, specificity, sensitivity, correlation, chi_square, negate
 
 __all__ = [
     'get_data', 'load_data', 'BeamSearch', 'search', 'weighted_relative_accuracy',
+    'specificity', 'sensitivity', 'correlation', 'chi_square', 'negate',
 ]
--- beamsearch/metrics.py
+++ beamsearch/metrics.py
@@ -1,4 +1,5 @@
 import functools
+import math
 import numpy as np
 
 
@@ -10,6 +11,43 @@ def weighted_relative_accuracy(X, y, subgroup):
     n = np.count_nonzero(~subY)
 
     return p/P - n/N
+
+
+def specificity(X, y, subgroup):
+    subY = y[subgroup]
+
+    N = np.count_nonzero(~y)
+    n = np.count_nonzero(~subY)
+
+    return 1 - n/N
+
+
+def sensitivity(X, y, subgroup):
+    subY = y[subgroup]
+    P = np.count_nonzero(y)
+    p = np.count_nonzero(subY)
+
+    return p/P
+
+
+def correlation(X, y, subgroup):
+    subY = y[subgroup]
+    P = np.count_nonzero(y)
+    p = np.count_nonzero(subY)
+    N = np.count_nonzero(~y)
+    n = np.count_nonzero(~subY)
+
+    if p + n == 0 or (P - p + N - n) == 0:
+        return 0
+
+    return (p * N - P * n) / math.sqrt(P * N * (p + n) * (P - p + N - n))
+
+
+def chi_square(X, y, subgroup):
+    P = np.count_nonzero(y)
+    N = np.count_nonzero(~y)
+
+    return (P + N) * (correlation(X, y, subgroup) ** 2)
 
 
 def negate(f):
