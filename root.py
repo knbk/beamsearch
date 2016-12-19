caa537e7140ab3daacb4f35cf8422724828eec7d
Luttik <dtluttik@gmail.com>
2016-12-19 12:20:41 +0100

    Removed old datasets and added the new ones (expect meta_data). Also removed some old code from root.py

--- root.py
+++ root.py
@@ -1,14 +1,5 @@
 from beamsearch import *
 from discrimination_search import find_discrimination
 
-data = get_data('./dataset.arff')
 
-searcher = BeamSearch(metric=semi_elift, q=40)
-subsets = searcher.search(data)
-print('x')
-with open('discrimination_subgroups.csv', 'w') as file:
-    file.write("measure;count;attributes\n")
-    for i in range(len(subsets)):
-        subset = subsets[i]
-        file.write(str(subset.measure) + ";" + str(subset.count) + ";" + str(subset.attributes).replace(',', ' and ') + "\n")
-# find_discrimination(data)
+open
