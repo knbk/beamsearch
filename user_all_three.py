c28230e03e5b7a4a3231852ab1c6a06aff748cd4
Sajid1995 <sajidmohideen1995@gmail.com>
2016-12-21 16:07:37 +0100

    Customers appearing in all three datasets

--- user_all_three.py
+++ user_all_three.py
@@ -9,14 +9,14 @@ exp_id = {}
 meta_id = {}
 user_id = {}
 for row in click_data.x:
-    click_id[row[4]] = 1
+    click_id[row[4]] = None
 for row in exp_details.x:
-    exp_id[row[0]]= 1
+    exp_id[row[0]]= None
 for row in meta_data.x:
-    meta_id[row[7]] = 1
+    meta_id[row[7]] = None
 
 for key in click_id:
     if key in exp_id and key in meta_id:
-        user_id[key] = 3
+        user_id[key] = None
 for key in user_id:
     print(key)
